ab527b873e162c4563fc83dc89258dab55d18603
myahal <miya.chiharu@gmail.com>
2020-02-06 11:04:43 +0900

    push, popを実装

--- src/com/hoge/HeapImpl.java
+++ src/com/hoge/HeapImpl.java
@@ -1,4 +1,50 @@
 package com.hoge;
 
+import java.util.ArrayList;
+
 public class HeapImpl {
+    private ArrayList<Integer> list = new ArrayList<>();
+
+    public void push(int element) {
+        int n = list.size();
+        list.add(element);
+        while(n != 0) {
+            int parent = (n - 1)/2;
+            if (list.get(parent) < list.get(n)) {
+                int tmp = list.get(parent);
+                list.set(parent, element);
+                list.set(n, tmp);
+            }
+            n = parent;
+        }
+    }
+
+    public int pop() {
+        int ret = list.get(0);
+        int n = list.size() - 1;
+        list.set(0, list.get(n));
+        list.remove(n);
+        n = 0;
+        for(int i = n * 2 + 1 ; i < list.size(); i = n * 2 + 1) {
+            int j = i + 1;
+            int index = i;
+            if (j < list.size() && list.get(i) < list.get(j)) {
+                index = j;
+            }
+            if (list.get(n) < list.get(index)) {
+                int tmp = list.get(n);
+                list.set(n, list.get(index));
+                list.set(index, tmp);
+            }
+            n = index;
+        }
+        return ret;
+    }
+
+    public void print() {
+        for(int v: list) {
+            System.out.print(v + ", ");
+        }
+        System.out.println();
+    }
 }
--- src/com/hoge/Main.java
+++ src/com/hoge/Main.java
@@ -3,6 +3,21 @@ package com.hoge;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        HeapImpl heap = new HeapImpl();
+        heap.push(10);
+        heap.push(5);
+        heap.push(7);
+        heap.push(2);
+        heap.push(1);
+        heap.push(20);
+        heap.push(15);
+        heap.print();
+        System.out.println(heap.pop());
+        System.out.println(heap.pop());
+        System.out.println(heap.pop());
+        System.out.println(heap.pop());
+        System.out.println(heap.pop());
+        System.out.println(heap.pop());
+        System.out.println(heap.pop());
     }
 }
